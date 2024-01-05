1afb3d967998ffa4880caa3ca60c842a4b54154c
EdsonCosta <edsonpaulojoaodacosta@gmail.com>
2024-01-05 00:07:14 +0000

    feat:create userService

---
+++ src/main/java/com/edsoncosta/paymentSystem/services/UserService.java
@@ -0,0 +1,40 @@
+package com.edsoncosta.paymentSystem.services;
+
+
+import com.edsoncosta.paymentSystem.domain.models.User;
+import com.edsoncosta.paymentSystem.domain.respositories.UserRepository;
+import com.edsoncosta.paymentSystem.utils.RandomString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) throws Exception {
+
+        if(userRepository.findByEmail(user.getEmail()) !=null )
+        {
+            throw new Exception("This email already exists!");
+        }
+
+        String encodedPassword=passwordEncoder.encode(user.getPassword());
+        String randomCode= RandomString.generateRandomString(64);
+
+        user.setPassword(encodedPassword);
+        user.setVerificatioCode(randomCode);
+        user.setEnabled(false);
+
+        User userSaved=this.userRepository.save(user);
+
+        return userSaved;
+    }
+
+
+}
