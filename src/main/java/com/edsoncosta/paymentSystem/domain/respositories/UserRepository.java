c5d1a227acfb00110ac3380ad30889d2219eb3ee
EdsonCosta <edsonpaulojoaodacosta@gmail.com>
2024-01-05 11:31:07 +0000

    fix:solving bug to query data by user status and app running

--- src/main/java/com/edsoncosta/paymentSystem/domain/respositories/UserRepository.java
+++ src/main/java/com/edsoncosta/paymentSystem/domain/respositories/UserRepository.java
@@ -4,11 +4,11 @@ import com.edsoncosta.paymentSystem.domain.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    UserDetails findByEmail(String email);
+    User findByEmail(String email);
     User findByName(String name);
-    User findByEnabled();
-    User findByDisabled();
+    List<User> findAllByEnabledIsTrue();
 }
