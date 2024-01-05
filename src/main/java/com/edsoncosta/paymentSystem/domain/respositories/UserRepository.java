6cb51bfcc40825c412f266ad2d031175571f86d8
EdsonCosta <edsonpaulojoaodacosta@gmail.com>
2024-01-05 00:06:07 +0000

    feat:create userRepository

---
+++ src/main/java/com/edsoncosta/paymentSystem/domain/respositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.edsoncosta.paymentSystem.domain.respositories;
+
+import com.edsoncosta.paymentSystem.domain.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    UserDetails findByEmail(String email);
+    User findByName(String name);
+    User findByEnabled();
+    User findByDisabled();
+}
