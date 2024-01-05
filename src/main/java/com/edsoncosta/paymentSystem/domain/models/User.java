a5333d2219e13328d8cf94e43ae435fe8a17a474
EdsonCosta <edsonpaulojoaodacosta@gmail.com>
2024-01-05 00:06:50 +0000

    feat:create new construtors

---
+++ src/main/java/com/edsoncosta/paymentSystem/domain/models/User.java
@@ -0,0 +1,76 @@
+package com.edsoncosta.paymentSystem.domain.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Data
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+
+    private String verificatioCode;
+
+    private boolean enabled;
+
+    public User(UUID id, String name, String email, String password, String verificatioCode, boolean enabled) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.verificatioCode = verificatioCode;
+        this.enabled = enabled;
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled()
+    {
+        return this.enabled;
+    }
+
+}
