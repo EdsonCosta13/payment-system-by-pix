18a2dd3ae4ec5a960f20f635f23887a9b5571d22
EdsonCosta <edsonpaulojoaodacosta@gmail.com>
2024-01-07 15:39:24 +0000

    feat:implements logics to verify email successfull

--- src/main/java/com/edsoncosta/paymentSystem/services/UserService.java
+++ src/main/java/com/edsoncosta/paymentSystem/services/UserService.java
@@ -5,9 +5,12 @@ import com.edsoncosta.paymentSystem.domain.dtos.UserResponseDTO;
 import com.edsoncosta.paymentSystem.domain.models.User;
 import com.edsoncosta.paymentSystem.domain.respositories.UserRepository;
 import com.edsoncosta.paymentSystem.utils.RandomString;
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
 
 @Service
 public class UserService {
@@ -18,24 +21,45 @@ public class UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public UserResponseDTO registerUser(User user) throws Exception {
+    @Autowired
+    private MailService mailService;
+
+    public UserResponseDTO registerUser(User user) throws MessagingException, UnsupportedEncodingException {
 
         if(userRepository.findByEmail(user.getEmail()) !=null )
         {
-            throw new Exception("This email already exists!");
+            throw new RuntimeException("This email already exists!");
         }
 
         String encodedPassword=passwordEncoder.encode(user.getPassword());
         String randomCode= RandomString.generateRandomString(64);
 
         user.setPassword(encodedPassword);
-        user.setVerificatioCode(randomCode);
+        user.setVerificationCode(randomCode);
         user.setEnabled(false);
 
         User userSaved=this.userRepository.save(user);
         UserResponseDTO userReponse=new UserResponseDTO(userSaved.getId(),userSaved.getName(),userSaved.getEmail(),userSaved.getPassword());
 
+        this.mailService.sendVerificationEmail(user);
         return userReponse;
+    }
+
+    public boolean verify(String verificationCode)
+    {
+        User user=this.userRepository.findByVerificationCode(verificationCode);
+
+        if(user==null || user.isEnabled())
+        {
+            return false;
+        }else{
+            user.setVerificationCode(null);
+            user.setEnabled(true);
+
+            this.userRepository.save(user);
+
+            return true;
+        }
     }
 
 
